a54cd9cb76c41aa91c04e129fb108c743e1d7187
jlopezjmaldonado25 <jlopez25.cursos@gmail.com>
2025-09-28 13:12:51 -0500

    configuracion aplicacion inicial

---
+++ src/app/tasks/index.ts
@@ -0,0 +1,6 @@
+// Re-export everything from the tasks module
+export * from './components';
+export * from './services';
+export * from './business';
+export * from './models';
+export * from './dtos';
